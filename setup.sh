7f51679a32214f5137ef58e09e8c276b5ace184f
Paul VanKeuren <paul.vankeuren@gmail.com>
2015-10-14 18:55:29 -0500

    revert changes

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ mkdir -p out
 echo "This is when I'd normally install emscripten"
 
 # Get portable emscripten
-#wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz -P ./emscripten
+wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz -P ./emscripten
 
 # Untar the downloaded file
-#tar xvzf emscripten/emsdk-portable.tar.gz -C ./emscripten
+tar xvzf emscripten/emsdk-portable.tar.gz -C ./emscripten
 
 # Update emscripten
-#./emscripten/emsdk_portable/emsdk update
-#./emscripten/emsdk_portable/emsdk install latest
-#./emscripten/emsdk_portable/emsdk activate latest
+./emscripten/emsdk_portable/emsdk update
+./emscripten/emsdk_portable/emsdk install latest
+./emscripten/emsdk_portable/emsdk activate latest
